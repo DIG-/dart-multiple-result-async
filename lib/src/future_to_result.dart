23e28b343da429ec0a770e97a0b3e9dae42f89c2
Gustavo de Oliveira Silva (DIG) <contato@dig.dev.br>
2025-05-16 00:25:00 -0300

    Removed try-catch from Future<T>.toResult(), alse increased performance using `then` instead of await.

--- lib/src/future_to_result.dart
+++ lib/src/future_to_result.dart
@@ -1,32 +1,28 @@
 import 'package:multiple_result/multiple_result.dart';
 
 extension FutureToResult<T> on Future<T> {
-  Future<Result<T, E>> _toResultWithoutTryCatch<U, E>() async {
-    return Result.success(await this);
-  }
-
-  Future<Result<T, E>> _toResultWithTryCatch<U, E>() async {
-    try {
-      return Result.success(await this);
-    } catch (error) {
-      if (error case final E error) {
-        return Error(error);
-      }
-      rethrow;
-    }
-  }
-
   @pragma('vm:prefer-inline')
-  Future<Result<T, E>> toResult<U, E>({final bool useTryCatch = true}) {
+  Future<Result<T, E>> toResult<U, E>() {
     assert(
       T == U,
       'The type of the Future must be the same as the type of the Result. T=$T, U=$U',
     );
-    return useTryCatch
-        ? _toResultWithTryCatch<U, E>()
-        : _toResultWithoutTryCatch<U, E>();
+    return then<Result<T, E>>(
+      // It is faster to use a lambda here
+      // ignore: unnecessary_lambdas
+      (final value) => Success<T, E>(value),
+    );
   }
 
   @pragma('vm:prefer-inline')
-  Future<Result<T, E>> toResultOrError<E>() => _toResultWithTryCatch<T, E>();
+  Future<Result<T, E>> toResultOrError<E>() {
+    return then<Result<T, E>>(
+      // It is faster to use a lambda here
+      // ignore: unnecessary_lambdas
+      (final value) => Success<T, E>(value),
+    ).catchError(
+      (final error) => Error<T, E>(error),
+      test: (final error) => error is E,
+    );
+  }
 }
--- test/future_to_result_test.dart
+++ test/future_to_result_test.dart
@@ -2,106 +2,77 @@ import 'package:multiple_result/multiple_result.dart';
 import 'package:multiple_result_async/multiple_result_async.dart';
 import 'package:test/test.dart';
 
-const String kTestString = 'Foo';
-
-Future<String> alwaysReturnString(final String message) async {
-  return message;
-}
-
-Future<String> alwaysThrowsException() async {
-  throw const FormatException('Stub');
-}
+import '_common.dart';
 
 void main() {
-  group('Future to Result with try-catch', () {
+  group('Future to Result', () {
     test('toResult with Success', () async {
-      final futureResult = alwaysReturnString(kTestString)
-          .toResult<String, Exception>(useTryCatch: true);
+      final futureResult =
+          alwaysFutureString(kDefaultTestString).toResult<String, Exception>();
       final result = await futureResult;
       expect(result, isA<Success<String, Exception>>());
-      expect((result as Success<String, Exception>).success, kTestString);
+      expect(
+        (result as Success<String, Exception>).success,
+        kDefaultTestString,
+      );
     });
 
     test('toResult with Exception and correct Type', () async {
-      final futureResult = alwaysThrowsException()
-          .toResult<String, Exception>(useTryCatch: true);
-      final result = await futureResult;
-      expect(result, isA<Error<String, Exception>>());
+      final futureResult =
+          alwaysFutureThrowsException().toResult<String, Exception>();
       expect(
-        (result as Error<String, Exception>).error,
-        isA<FormatException>(),
+        () async => await futureResult,
+        throwsA(isA<DefaultTestException>()),
       );
     });
 
     test('toResult with Exception and wrong Type', () async {
       final futureResult =
-          alwaysThrowsException().toResult<String, String>(useTryCatch: true);
-      expect(() async => await futureResult, throwsA(isA<FormatException>()));
-    });
-
-    test('toResult with unmatched input and output types', () async {
+          alwaysFutureThrowsException().toResult<String, String>();
       expect(
-        () => alwaysReturnString(kTestString)
-            .toResult<int, Exception>(useTryCatch: true),
-        throwsA(isA<AssertionError>()),
+        () async => await futureResult,
+        throwsA(isA<DefaultTestException>()),
       );
     });
-  });
-
-  group('Future to Result without try-catch', () {
-    test('toResult with Success', () async {
-      final futureResult = alwaysReturnString(kTestString)
-          .toResult<String, Exception>(useTryCatch: false);
-      final result = await futureResult;
-      expect(result, isA<Success<String, Exception>>());
-      expect((result as Success<String, Exception>).success, kTestString);
-    });
-
-    test('toResult with Exception and correct Type', () async {
-      final futureResult = alwaysThrowsException()
-          .toResult<String, Exception>(useTryCatch: false);
-      expect(() async => await futureResult, throwsA(isA<FormatException>()));
-    });
-
-    test('toResult with Exception and wrong Type', () async {
-      final futureResult =
-          alwaysThrowsException().toResult<String, String>(useTryCatch: false);
-      expect(() async => await futureResult, throwsA(isA<FormatException>()));
-    });
 
     test('toResult with unmatched input and output types', () async {
       expect(
-        () => alwaysReturnString(kTestString)
-            .toResult<int, Exception>(useTryCatch: false),
+        () => alwaysFutureString(kDefaultTestString).toResult<int, Exception>(),
         throwsA(isA<AssertionError>()),
       );
     });
   });
 
   group('Future to Result or Error', () {
-    // Same as toResult with try-catch
-
     test('toResult with Success', () async {
       final futureResult =
-          alwaysReturnString(kTestString).toResultOrError<Exception>();
+          alwaysFutureString(kDefaultTestString).toResultOrError<Exception>();
       final result = await futureResult;
       expect(result, isA<Success<String, Exception>>());
-      expect((result as Success<String, Exception>).success, kTestString);
+      expect(
+        (result as Success<String, Exception>).success,
+        kDefaultTestString,
+      );
     });
 
     test('toResult with Exception and correct Type', () async {
-      final futureResult = alwaysThrowsException().toResultOrError<Exception>();
+      final futureResult =
+          alwaysFutureThrowsException().toResultOrError<Exception>();
       final result = await futureResult;
       expect(result, isA<Error<String, Exception>>());
       expect(
         (result as Error<String, Exception>).error,
-        isA<FormatException>(),
+        isA<DefaultTestException>(),
       );
     });
 
     test('toResult with Exception and wrong Type', () async {
-      final futureResult = alwaysThrowsException().toResultOrError<String>();
-      expect(() async => await futureResult, throwsA(isA<FormatException>()));
+      final futureResult =
+          alwaysFutureThrowsException().toResultOrError<String>();
+      expect(
+        () async => await futureResult,
+        throwsA(isA<DefaultTestException>()),
+      );
     });
   });
 }
