b44133bf8aed5c049fc67da2885599a3ac21655a
Gustavo de Oliveira Silva (DIG) <contato@dig.dev.br>
2025-05-18 23:20:51 -0300

    Update FutureOr<Result<T,E>>.next() using existing error handling

--- lib/src/next_future_result.dart
+++ lib/src/next_future_result.dart
@@ -2,57 +2,33 @@ import 'dart:async';
 
 import 'package:multiple_result/multiple_result.dart';
 
-import 'next_result.dart';
-import 'util.dart';
-
 extension NextFutureResult<T, E> on Future<Result<T, E>> {
-  Future<Result<U, E>> _nextWithoutTryCatch<U>({
-    required final FutureOr<Result<U, E>> Function(T value) onSuccess,
-    final FutureOr<Result<U, E>> Function(E errror)? onError,
-  }) async {
-    return await (await this).next(
-      onSuccess: onSuccess,
-      onError: onError,
-      useTryCatch: false,
-    );
-  }
-
-  Future<Result<U, E>> _nextWithTryCatch<U>({
-    required final FutureOr<Result<U, E>> Function(T value) onSuccess,
-    final FutureOr<Result<U, E>> Function(E errror)? onError,
-  }) async {
-    final Result<T, E> result;
-    try {
-      result = await this;
-    } catch (error) {
-      if (error case final E error) {
-        if (onError != null) {
-          return await ErrorHandler.handle(error, onError);
-        }
-        return Error(error);
-      }
-      rethrow;
-    }
-    return await result.next(
-      onSuccess: onSuccess,
-      onError: onError,
-      useTryCatch: true,
-    );
-  }
-
-  @pragma('vm:prefer-inline')
   Future<Result<U, E>> next<U>({
     required final FutureOr<Result<U, E>> Function(T value) onSuccess,
     final FutureOr<Result<U, E>> Function(E errror)? onError,
-    final bool useTryCatch = true,
-  }) =>
-      useTryCatch
-          ? _nextWithTryCatch(
-              onSuccess: onSuccess,
-              onError: onError,
-            )
-          : _nextWithoutTryCatch(
-              onSuccess: onSuccess,
-              onError: onError,
-            );
+  }) {
+    // Avoiding expliciity try-catch and async-await is faster here, since Future has it own exception handling
+    var skipOuterCatch = false;
+    return then((final result) {
+      switch (result) {
+        case Success<T, E>(success: final T value):
+          return onSuccess(value);
+        case Error<T, E>(error: final E error):
+          skipOuterCatch = true;
+          if (onError != null) {
+            return onError(error);
+          }
+          return Error<U, E>(error);
+      }
+    })
+        .catchError(
+          (final error) => onError!(error as E),
+          test: (final error) =>
+              !skipOuterCatch && onError != null && error is E,
+        )
+        .catchError(
+          (final error) => Error<U, E>(error),
+          test: (final error) => error is E,
+        );
+  }
 }
--- lib/src/next_result.dart
+++ lib/src/next_result.dart
@@ -2,54 +2,12 @@ import 'dart:async';
 
 import 'package:multiple_result/multiple_result.dart';
 
-import 'util.dart';
+import 'next_future_result.dart';
 
 extension NextResult<T, E> on Result<T, E> {
-  Future<Result<U, E>> _nextWithoutTryCatch<U>({
-    required final FutureOr<Result<U, E>> Function(T value) onSuccess,
-    final FutureOr<Result<U, E>> Function(E errror)? onError,
-  }) async {
-    return switch (this) {
-      Success<T, E>(success: final T value) => await onSuccess(value),
-      Error<T, E>(error: final E error) =>
-        onError == null ? Error(error) : await onError(error),
-    };
-  }
-
-  Future<Result<U, E>> _nextWithTryCatch<U>({
-    required final FutureOr<Result<U, E>> Function(T value) onSuccess,
-    final FutureOr<Result<U, E>> Function(E errror)? onError,
-  }) async {
-    try {
-      return await _nextWithoutTryCatch(
-        onSuccess: onSuccess,
-        onError: onError,
-      );
-    } catch (error) {
-      if (error case final E error) {
-        if (onError != null) {
-          return await ErrorHandler.handle(error, onError);
-        } else {
-          return Error(error);
-        }
-      }
-      rethrow;
-    }
-  }
-
-  @pragma('vm:prefer-inline')
   Future<Result<U, E>> next<U>({
     required final FutureOr<Result<U, E>> Function(T value) onSuccess,
     final FutureOr<Result<U, E>> Function(E errror)? onError,
-    final bool useTryCatch = true,
   }) =>
-      useTryCatch
-          ? _nextWithTryCatch(
-              onSuccess: onSuccess,
-              onError: onError,
-            )
-          : _nextWithoutTryCatch(
-              onSuccess: onSuccess,
-              onError: onError,
-            );
+      Future.value(this).next(onSuccess: onSuccess, onError: onError);
 }
